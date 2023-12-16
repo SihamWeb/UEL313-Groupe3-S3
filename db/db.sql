3dcd2cbbc12fa4de250e7114218af4045d4a0455
Valentine <89517413+Valou38@users.noreply.github.com>
2023-12-16 17:24:21 +0100

    frant-end-branch : Ajout liens in database

--- db/db.sql
+++ db/db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 172.17.0.2
--- Généré le : ven. 15 déc. 2023 à 16:55
--- Version du serveur : 8.0.35-0ubuntu0.20.04.1
--- Version de PHP : 8.1.26
+-- Hôte : localhost
+-- Généré le : sam. 16 déc. 2023 à 17:23
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tl_liens` (
-  `lien_id` int NOT NULL,
-  `lien_url` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `lien_titre` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `lien_desc` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci,
-  `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+  `lien_id` int(11) NOT NULL,
+  `lien_url` varchar(255) NOT NULL,
+  `lien_titre` varchar(255) NOT NULL,
+  `lien_desc` text DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_liens`
@@ -47,7 +47,12 @@ INSERT INTO `tl_liens` (`lien_id`, `lien_url`, `lien_titre`, `lien_desc`, `user_
 (13, 'https://www.wampserver.com/', 'WampServer', 'WampServer est une plate-forme de développement Web sous Windows pour des applications Web dynamiques à l’aide du serveur Apache2, du langage de scripts PHP et d’une base de données MySQL.', 3),
 (14, 'https://www.net-concept.fr/developpement-web/le-developpement-web-cest-quoi/', 'Net Concept', 'Le développement Web désigne de manière générale les tâches associées au développement de sites Web destinés à être hébergés via un intranet ou Internet.', 3),
 (15, 'https://www.cnil.fr/fr', 'CNIL', 'Le site de la CNIL offre des informations sur vos droits en matière de données personnelles et comment les exercer. Il propose également des conseils sur la protection de vos données sur Internet.', 3),
-(16, 'https://www.ionos.fr', 'IONOS', 'IONOS est un fournisseur de services Internet offrant des solutions d’hébergement, de création de sites Web et de commerce en ligne.', 3);
+(16, 'https://www.ionos.fr', 'IONOS', 'IONOS est un fournisseur de services Internet offrant des solutions d’hébergement, de création de sites Web et de commerce en ligne.', 3),
+(20, 'https://www.letelegramme.fr/', 'Le Télégramme', 'Le Télégramme est un média régional français de Bretagne, dont le siège se trouve à Morlaix. Sa version imprimée est diffusée dans le Finistère, les Côtes-d\'Armor, le Morbihan et dans le pays de Saint-Malo. Il emploie 550 personnes, dont près de 220 journalistes.', 8),
+(21, 'https://cnes.fr/fr', 'CNES', 'Le Centre national d\'études spatiales est un établissement public à caractère industriel et commercial chargé d’élaborer et de proposer au gouvernement français le programme spatial français, puis de le mettre en œuvre.', 8),
+(23, 'https://www.legifrance.gouv.fr/', 'Legifrance', 'Légifrance est le site web officiel du gouvernement français pour la diffusion des textes législatifs et réglementaires et des décisions de justice des cours suprêmes et d\'appel de droit français', 8),
+(24, 'https://www.reddit.com/', 'Reddit', 'Reddit est un site web communautaire américain de discussion et d’actualités sociales. Les liens les plus appréciés du moment se trouvent affichés en page d’accueil par le vote des utilisateurs. Fondé en 2005, Reddit contient alors surtout du contenu sur la programmation et la science.', 8),
+(25, 'https://www.lemonde.fr/', 'Le Monde', 'Le Monde est un quotidien français de l\'après-midi. C\'est la principale publication du groupe Le Monde et a fait état d\'un tirage moyen de 479 243 exemplaires par numéro en 2022, dont environ 40 000 ont été vendus à l\'étranger.', 8);
 
 -- --------------------------------------------------------
 
@@ -56,9 +61,9 @@ INSERT INTO `tl_liens` (`lien_id`, `lien_url`, `lien_titre`, `lien_desc`, `user_
 --
 
 CREATE TABLE `tl_tags` (
-  `tag_id` int NOT NULL,
-  `tag_name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+  `tag_id` int(11) NOT NULL,
+  `tag_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_tags`
@@ -86,7 +91,17 @@ INSERT INTO `tl_tags` (`tag_id`, `tag_name`) VALUES
 (22, 'codage'),
 (23, 'données'),
 (24, 'hébergement'),
-(25, 'googleg');
+(25, 'googleg'),
+(26, 'Informations'),
+(27, 'Bretagne'),
+(28, 'Journal'),
+(29, 'Astronomie'),
+(30, 'Spatial'),
+(31, 'France'),
+(32, 'Droit'),
+(33, 'Justice'),
+(34, 'Informatique'),
+(35, 'Social');
 
 -- --------------------------------------------------------
 
@@ -95,9 +110,9 @@ INSERT INTO `tl_tags` (`tag_id`, `tag_name`) VALUES
 --
 
 CREATE TABLE `tl_tags_liens` (
-  `tag_id` int NOT NULL,
-  `lien_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+  `tag_id` int(11) NOT NULL,
+  `lien_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_tags_liens`
@@ -138,7 +153,27 @@ INSERT INTO `tl_tags_liens` (`tag_id`, `lien_id`) VALUES
 (13, 16),
 (24, 16),
 (5, 17),
-(25, 18);
+(25, 18),
+(26, 19),
+(27, 19),
+(28, 19),
+(26, 20),
+(27, 20),
+(28, 20),
+(29, 22),
+(30, 22),
+(6, 22),
+(31, 23),
+(32, 23),
+(33, 23),
+(34, 24),
+(35, 24),
+(26, 25),
+(28, 25),
+(31, 25),
+(29, 21),
+(30, 21),
+(6, 21);
 
 -- --------------------------------------------------------
 
@@ -147,12 +182,12 @@ INSERT INTO `tl_tags_liens` (`tag_id`, `lien_id`) VALUES
 --
 
 CREATE TABLE `tl_users` (
-  `usr_id` int NOT NULL,
-  `usr_name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `usr_password` varchar(88) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `usr_salt` varchar(23) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `usr_role` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'ROLE_ADMIN'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+  `usr_id` int(11) NOT NULL,
+  `usr_name` varchar(50) NOT NULL,
+  `usr_password` varchar(88) NOT NULL,
+  `usr_salt` varchar(23) NOT NULL,
+  `usr_role` varchar(50) NOT NULL DEFAULT 'ROLE_ADMIN'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_users`
@@ -161,7 +196,8 @@ CREATE TABLE `tl_users` (
 INSERT INTO `tl_users` (`usr_id`, `usr_name`, `usr_password`, `usr_salt`, `usr_role`) VALUES
 (1, 'admin', 'LsJKppRTEPz4uKrkhScOE6HBSvHuaIcFbAX9FWC7h/f5HffX4TBcFt7p8M0hqvGzFXL+JV8TzEYePoimaosfMQ==', '>=28!7NLw!S37zLjs7Uu[nC', 'ROLE_ADMIN'),
 (3, 'siham', 'x7f2YI75e/YZiElXZlkDlNAQD52NNMx/Wu3lx09V9PoDvk+0v63FewiTuuyCVjkh4owv9UZKL8RWgrJS6yF3Uw==', '262583b7564f4e36511f8bc', 'ROLE_ADMIN'),
-(6, 'watson', 'n1jHV3sIhxeEReVLnIZRibFM8q9AhBS6wzgPyz/YveNF7Q7h/hQ08hbMaT2aeKyVIo2A8SqrVw/SXfOEaPcuDQ==', '78627d9acf81fa84ab8f5c5', 'ROLE_ADMIN');
+(6, 'watson', 'n1jHV3sIhxeEReVLnIZRibFM8q9AhBS6wzgPyz/YveNF7Q7h/hQ08hbMaT2aeKyVIo2A8SqrVw/SXfOEaPcuDQ==', '78627d9acf81fa84ab8f5c5', 'ROLE_ADMIN'),
+(8, 'valentine', 'Ulic8XEACHw8DQg6NEEYGj+9CH8r9IpUekyW+Kx8l8LMuA2l/QN8Cj/OP8Th3lQwbQZz/DiPay15bRPWuikrVg==', '38d8e79ddac6df3f2753b50', 'ROLE_ADMIN');
 
 --
 -- Index pour les tables déchargées
@@ -193,19 +229,19 @@ ALTER TABLE `tl_users`
 -- AUTO_INCREMENT pour la table `tl_liens`
 --
 ALTER TABLE `tl_liens`
-  MODIFY `lien_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `lien_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `tl_tags`
 --
 ALTER TABLE `tl_tags`
-  MODIFY `tag_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT pour la table `tl_users`
 --
 ALTER TABLE `tl_users`
-  MODIFY `usr_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
